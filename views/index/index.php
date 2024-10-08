740db16b2c33dc7297eb39f4074d2292d38df44e
Djordje Petrovic <djordje.pojatarac95@gmail.com>
2024-10-08 14:37:58 +0200

    fix

--- views/index/index.php
+++ views/index/index.php
@@ -2,7 +2,7 @@
 <?php require_once __DIR__ . '/../layouts/header.php'; ?>
 
 <div class="d-block text-center mt-5" style="height: 100vh">
-    <h1>Welcome to <?= config('app.app_name') ?></h1>
+    <h1>Welcome to the <?= config('app.app_name') ?></h1>
     <img src="<?= public_dir('images/main.jpg') ?>" alt="image" class="img-fluid rounded mx-auto d-block" style="max-height: 80vh">
 </div>
 
